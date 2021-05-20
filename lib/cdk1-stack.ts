a5621b999c8f7fb34fbf9f4880af02e2583944f5
dev <luigi0229@gmail.com>
2021-05-20 00:42:20 -0400

    fixed the validators

--- lib/cdk1-stack.ts
+++ lib/cdk1-stack.ts
@@ -3,6 +3,7 @@ import * as apigateway from "@aws-cdk/aws-apigateway";
 import * as dynamodb from "@aws-cdk/aws-dynamodb";
 import * as lambda from "@aws-cdk/aws-lambda";
 
+
 //import required modules from:
 // https://docs.aws.amazon.com/cdk/api/latest/docs/aws-construct-library.html
 
@@ -34,6 +35,8 @@ export class Cdk1Stack extends cdk.Stack {
     //create API GW
     const api = new apigateway.RestApi(this, 'products-api');
 
+
+
     api.root
     .resourceForPath('products')
     .addMethod("GET", new apigateway.LambdaIntegration(dynamoLambdaFn))
@@ -42,13 +45,54 @@ export class Cdk1Stack extends cdk.Stack {
     // .resourceForPath('product')
     // .addMethod("GET", new apigateway.LambdaIntegration(dynamoLambdaFn))
 
+    const requestValidator = new apigateway.RequestValidator(this, "MyPayloadValidator", {
+            restApi: api,
+            // requestValidatorName: `myproj-prod-payload-validator`,
+            validateRequestBody: true,
+            validateRequestParameters: false,
+        });
+
     api.root
     .resourceForPath('products')
-    .addMethod("POST", new apigateway.LambdaIntegration(dynamoLambdaFn))
+    .addMethod("POST", new apigateway.LambdaIntegration(dynamoLambdaFn), {
+      requestValidator: requestValidator,
+      requestModels: {"application/json": new apigateway.Model(this, "modelFromCDK", {
+        restApi: api,
+        schema:  {
+          "title": "Product",
+          "properties": {
+            "price": {
+              "description": "Price of the product",
+              "type": apigateway.JsonSchemaType.NUMBER,
+              "minimum": 0
+            },
+            "name": {
+              "description": "name of the product",
+              "type": apigateway.JsonSchemaType.STRING,
+              "maxLength": 40
+            },
+            "tags": {
+              "description": "Tags for the product",
+              "type": apigateway.JsonSchemaType.ARRAY,
+              "items": {
+                "type": apigateway.JsonSchemaType.STRING,
+                "minLength": 1
+              },
+              "minItems": 0,
+              "uniqueItems": true
+            }
+          },
+          "required": [ "id", "price", "name"]
+        }
+      }) }
+
+    })
 
     api.root
     .resourceForPath('search')
     .addMethod("POST", new apigateway.LambdaIntegration(dynamoLambdaFn))
+
+
 
     new cdk.CfnOutput(this, "HTTP API URL", {
       value: api.url ?? "Something failed"
