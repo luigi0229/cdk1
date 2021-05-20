3696548664293f8a19a3c63043e6417e1150f8fe
Eli Silvestre <luigi0229@gmail.com>
2021-05-19 20:49:34 -0400

    Add files via upload
    Having issues with cli, doing manually.

---
+++ src/models.js
@@ -0,0 +1,27 @@
+{
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "title": "Product",
+  "properties": {
+    "price": {
+      "description": "Price of the product",
+      "type": "number",
+      "minimum": 0
+    },
+    "name": {
+      "description": "name of the product",
+      "type": "string",
+      "maxLength": 40
+    },
+    "tags": {
+      "description": "Tags for the product",
+      "type": "array",
+      "items": {
+        "type": "string",
+        "minLength": 1
+      },
+      "minItems": 1,
+      "uniqueItems": true
+    }
+  },
+  "required": [ "id", "price", "name"]
+}
